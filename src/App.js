8da5946c992125b02bb01ef6978303f44f1954b5
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-10-20 20:40:48 +0900

    README 재수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // 라이브러리 불러오기
-const { Console } = require("@woowacourse/mission-utils");
-const GameLogic = require('./GameLogic')
+import { Console } from "@woowacourse/mission-utils";
+import GameLogic from './GameLogic'
 
 class App extends GameLogic {
   constructor() {
-    this.initialization
+    this.initialization();
   }
+
   async play() {
   }
 }
+
+const App = new App;
 
 export default App;
--- src/GameLogic.js
+++ src/GameLogic.js
@@ -1,19 +1,18 @@
-const { Random } = require("@woowacourse/mission-utils");
+import { Random } from "@woowacourse/mission-utils";
 
 class GameLogic {
   generateRandomNumber() {
-    const randomNumber = [];
+    const answerRandomNumber = [];
 
-    while (randomNumber.length < 3) {
-      const number = Random.pickNumberInRange(1, 9);
-      if (!randomNumber.includes(number)) {
-        randomNumber.push(number);
+    while (answerRandomNumber.length < 3) {
+      const randomNumber = Random.pickNumberInRange(1, 9);
+      if (!answerRandomNumber.includes(randomNumber)) {
+        answerRandomNumber.push(randomNumber);
       }
     }
 
-    return randomNumber;
+    return answerRandomNumber;
   }
 }
-
 
 module.exports = GameLogic;
