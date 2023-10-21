f05e2ce280da63e9e9121a4009dc4acad2de0f6e
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-10-21 22:42:53 +0900

    App.js.에서 게임 플레이 간이작성, GameLogic.js 예상값, 유효성검사 작성

--- src/GameLogic.js
+++ src/GameLogic.js
@@ -1,15 +1,11 @@
-import { Random } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class GameLogic {
-  constructor () {
-    this.secretNumber = this.generateRandomNumber();
-  }
-
   generateRandomNumber() {
     const answerRandomNumber = [];
 
     while (answerRandomNumber.length < 3) {
-      const number = Random.pickNumberInRange(1, 9);
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!answerRandomNumber.includes(number)) {
         answerRandomNumber.push(number);
       }
@@ -18,32 +14,60 @@ class GameLogic {
     return answerRandomNumber;
   }
 
-  countingStrike(guess) {
-    const guessNumber = guess.toString().split('').map(Number);
+  countingStrike(answerNumber, playerInputNumber) {
     let strike = 0;
 
     for (let i = 0; i < 3; i++) {
-      if (guessNumber[i] === this.answerRandomNumber[i]) {
-        strike ++;
+      if (answerNumber[i] === Number(playerInputNumber[i])) {
+        strike++;
       }
     }
 
     return strike;
   }
 
-  countingBall(guess) {
-    const guessNumber = guess.toString().split('').map(Number);
+  countingBall(answerNumber, playerInputNumber) {
     let ball = 0;
 
     for (let i = 0; i < 3; i++) {
-      if (guessNumber[i] === this.answerRandomNumber[i]) {
-        ball ++;
+      if (
+        answerNumber[i] !== Number(playerInputNumber[i]) &&
+        answerNumber.includes(Number(playerInputNumber[i]))
+      ) {
+        ball++;
       }
     }
 
     return ball;
   }
 
-}
+  showGuessResult(ball, strike) {
+    if (ball !== 0 && strike !== 0) {
+      return MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    }
+    if (ball !== 0 && strike === 0) {
+      return MissionUtils.Console.print(`${ball}볼`);
+    }
+    if (ball === 0 && strike === 0) {
+      return MissionUtils.Console.print("낫싱");
+    }
+  }
 
+  isValidInputData(playerInputNumber) {
+    const numberInputToSet = new Set(playerInputNumber.split("").map(Number));
+    if (playerInputNumber.length !== 3) {
+      return MissionUtils.Console.print("숫자는 3개를 입력해 주세요!");
+    }
+    if ([...numberInputToSet].length !== 3) {
+      return MissionUtils.Console.print("이런! 중첩되는 숫자가 존재합니다.");
+    }
+    if (playerInputNumber.includes(" ")) {
+      return MissionUtils.Console.print("공백은 제거해주세요!");
+    }
+    if (Number.isNaN(playerInputNumber)) {
+      return MissionUtils.Console.print("숫자만 입력주세요!");
+    }
+    return 'GoodToGo';
+  }
+}
 module.exports = GameLogic;
